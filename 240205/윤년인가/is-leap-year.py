7e0fb3b1291cd5885c85457b5cea70b10c23b137
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-05 22:18:09 +0900

    Title: 윤년인가, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/윤년인가/is-leap-year.py
+++ 240205/윤년인가/is-leap-year.py
@@ -1,3 +1,3 @@
 y = int(input())
 
-print("true") if y % 4 == 0 or (y % 4 == 0 and y % 100 == 0 and y % 400 == 0) else print("false")
+print("true") if (y % 4 == 0 and y % 100 != 0) or (y % 4 == 0 and y % 100 == 0 and y % 400 == 0) else print("false")
