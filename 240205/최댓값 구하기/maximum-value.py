1f0a87da924fa22f985754fcdba6ade9237143ec
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-05 22:27:10 +0900

    Title: 최댓값 구하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240205/최댓값 구하기/maximum-value.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+if a >= b:
+    if b > c or a > c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b > c or a > c:
+        print(b)
+    else:
+        print(c)
