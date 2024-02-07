008fed0e0fb2a400f13a20403fed13dc38f1f1de
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-07 23:23:38 +0900

    Title: 369 게임, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/369 게임/369-game.py
+++ 240207/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0 or str(i) in ["3", "6", "9"]:
+    if i % 3 == 0 or any(c in str(i) for c in ["3", "6", "9"]):
         print("0", end=" ")
     else:
         print(i, end=" ")
