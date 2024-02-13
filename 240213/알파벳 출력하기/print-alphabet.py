53c3ff09692982914143edf5a73e6974de846b84
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-13 23:35:25 +0900

    Title: 알파벳 출력하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/알파벳 출력하기/print-alphabet.py
+++ 240213/알파벳 출력하기/print-alphabet.py
@@ -5,5 +5,5 @@ for i in range(n):
     for _ in range(i+1):
         print(char, end="")
 
-        char = chr(ord(char) + 1) if ord(char) + 1 % 90 else "A"
+        char = chr(ord(char) + 1) if ord(char) < 90 else "A"
     print()
