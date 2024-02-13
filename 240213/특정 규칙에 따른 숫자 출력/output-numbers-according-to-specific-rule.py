d774c55853ce3d5995248da7dd8eb9d372e5a552
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-13 23:06:02 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240213/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if i > j:
+            print(" ", end=" ")
+        else:
+            cnt += 1
+            cnt = cnt % 10 if cnt % 10 else 1
+            print(cnt, end=" ")
+    print()
