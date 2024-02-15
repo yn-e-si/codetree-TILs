61133718dbdcf938e16b5998be98de3cfe2570b7
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-15 23:09:01 +0900

    Title: 학점 계산기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/학점 계산기/credit-calculator.py
+++ 240215/학점 계산기/credit-calculator.py
@@ -4,9 +4,9 @@ avg_grade = round(sum(arr) / len(arr), 1)
 
 print(f"{avg_grade}")
 
-if avg_grade > 4.0:
+if avg_grade >= 4.0:
     print("Perfect")
-elif avg_grade > 3.0:
+elif avg_grade >= 3.0:
     print("Good")
 else:
     print("Poor")
