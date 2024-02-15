b726fbd052f9824217d96795aeaf2f4140a8227a
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-15 23:24:35 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240215/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -3,7 +3,7 @@ pass_cnt = 0
 
 for i in range(n):
     arr = list(map(int, input().split()))
-    avg_score = round(sum(arr)/len(arr))
+    avg_score = sum(arr)//len(arr)
     if avg_score >= 60:
         pass_cnt += 1
         print("pass")
