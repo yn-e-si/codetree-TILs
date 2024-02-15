e71ec545d70d4279dd70fdd61abe03148812fad3
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2024-02-15 21:45:15 +0900

    Title: 완전수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/완전수/perfect-number.py
@@ -0,0 +1,21 @@
+start, end = map(int, input().split())
+ans = 0
+for i in range(start, end+1):
+    sum_val = 1
+    limit_num = int(i // 2)
+    print("i: ", i, "limit_num: ", limit_num)
+    check_num = 2
+
+    while check_num < limit_num:
+        if i  % check_num == 0:
+            sum_val += check_num
+            sum_val += i // check_num
+        
+        check_num += 1
+    
+    if sum_val == i:
+        print("i: ", i)
+        ans += 1
+
+
+print(ans)
